25e37f16a65821090dbb546c6fcf654a04efbf60
Eduardo <eduardo.ygn@gmail.com>
2026-05-11 22:19:33 +0300

    init

---
+++ auction-service/src/test/java/com/bidly/auctionservice/AuctionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bidly.auctionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
