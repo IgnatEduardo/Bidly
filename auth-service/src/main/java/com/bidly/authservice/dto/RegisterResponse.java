74237933f3181004f9bceb459cbbfd48e036525e
Pantarelo <118564862+Pantarelo@users.noreply.github.com>
2026-05-19 22:36:00 +0300

    dto: register, login

---
+++ auth-service/src/main/java/com/bidly/authservice/dto/RegisterResponse.java
@@ -0,0 +1,11 @@
+package com.bidly.authservice.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class RegisterResponse {
+    private String message;
+    private String email;
+}
