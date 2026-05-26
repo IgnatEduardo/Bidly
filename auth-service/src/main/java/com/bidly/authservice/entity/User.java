60906a8f35069e18fa681b81df5c76eb1802f0af
Pantarelo <118564862+Pantarelo@users.noreply.github.com>
2026-05-26 16:44:14 +0300

    refactor and demo controller for testing

--- auth-service/src/main/java/com/bidly/authservice/entity/User.java
+++ auth-service/src/main/java/com/bidly/authservice/entity/User.java
@@ -3,11 +3,13 @@ package com.bidly.authservice.entity;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Data
 @Builder
@@ -51,7 +53,9 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return rolename.stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName().name()))
+                .collect(Collectors.toList());
     }
 
     @Override
