c4a04a0b41ccd0c8851dfaef0fcbf9b0b17221cd
Eduardo <eduardo.ygn@gmail.com>
2026-06-21 18:57:50 +0300

    bidding-engine remove hardcoded feign client url

--- auction-service/src/main/java/com/bidly/auctionservice/client/UserClient.java
+++ auction-service/src/main/java/com/bidly/auctionservice/client/UserClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "auth-service", url = "http://localhost:8081")
+@FeignClient(name = "auth-service")
 public interface UserClient {
 
     @GetMapping("/api/v1/users/{id}")
