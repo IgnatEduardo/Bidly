1861cd64dee86e55235dbc8d8badcfc9fa490eed
Eduardo <eduardo.ygn@gmail.com>
2026-06-25 21:00:36 +0300

    ci integration tests removed

--- auction-service/src/main/java/com/bidly/auctionservice/client/UserClient.java
+++ auction-service/src/main/java/com/bidly/auctionservice/client/UserClient.java
@@ -5,7 +5,6 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-//test
 @FeignClient(name = "auth-service")
 public interface UserClient {
 
